ae4f90c111dc5c1ee40a97cd9c6a56f5ce3c05cb
shravani <lakshmisravani597@gmail.com>
2017-08-29 17:56:15 +0530

    validations passed to the booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -30,6 +30,7 @@ class BookingsController < ApplicationController
 
     respond_to do |format|
       if @booking.save
+        #binding.pry
         format.html { redirect_to @booking, notice: 'Booking was successfully created.' }
         format.json { render :show, status: :created, location: @booking }
       else
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,5 +1,16 @@
 class Booking < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :room
-
+	validate :booking_room
+	private
+	def booking_room
+		bookings = Booking.where('room_id=?',self.room_id)
+		new_dates = (self.start_date.to_datetime..self.end_date.to_datetime).to_a
+		bookings.each do |book|
+			previous_dates = (book.start_date.to_datetime..book.end_date.to_datetime).to_a
+				if (self.end_date < Date.today) && (previous_dates - new_dates).length != previous_dates.length			
+				self.errors.add(:base, "room is not available for booking")
+			end
+		end
+	end
 	end
